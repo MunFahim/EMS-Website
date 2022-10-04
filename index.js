c81ac3f8dc4a78091c840f60e96324b981ec458f
MunFahim <muntasirf362@gmail.com>
2022-10-04 18:57:50 -0400

    work in progress

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+
+let check = true;
+
 function showDrop(){
-    
+    const dBtn = document.getElementById("icon");
+    const dContent = document.getElementById("dropDownContent");
+    if (check){
+        dBtn.style.rotate = "90deg";
+        dContent.style.visibility = "visible";
+        
+    }else {
+        dBtn.style.rotate = "0deg";
+        dContent.style.visibility = "hidden";
+        
+    }
+    check = !check;
 }
